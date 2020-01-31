a9a95b2743f1799e60bc7f4310b126ea63e4ccdb
Wim Muskee <wimmuskee@gmail.com>
2020-01-31 15:03:51 +0100

    make setup pypi ready, test add 3.8 drop 3.4

--- setup.py
+++ setup.py
@@ -15,6 +15,15 @@ setup(
     keywords = "eduterm client",
     packages=find_packages(),
     long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kennisnet/py-eduterm-client",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries"
+    ],
     install_requires = [
          'requests'
          ]
